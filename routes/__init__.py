e35aa684d5f999f6972a83957a13302acbdd0d74
Jonathan Lane <admiralorbiter@gmail.com>
2025-03-19 08:56:03 -0500

    data in action page

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,9 +2,11 @@ from .auth import auth_bp
 from .main import main_bp
 from .dashboard import dashboard_bp
 from .upcoming_events import upcoming_events_bp
+from .dia import dia_events_bp
 
 def init_routes(app):
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(dashboard_bp)
-    app.register_blueprint(upcoming_events_bp, url_prefix='/events') 
+    app.register_blueprint(upcoming_events_bp, url_prefix='/events')
+    app.register_blueprint(dia_events_bp, url_prefix='/events') 
