287e3fd12cf1a4031c59cbdda6d2c4424778e838
Jon Lane <admiralorbiter@gmail.com>
2025-03-19 12:07:58 -0500

    started working on distirct pages for events

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,6 +4,9 @@ from .dashboard import dashboard_bp
 from .upcoming_events import upcoming_events_bp
 from .dia import dia_events_bp
 from .school_mappings import bp as school_mappings_bp
+from .district import bp as district_bp
+
+__all__ = ['init_routes']
 
 def init_routes(app):
     app.register_blueprint(main_bp)
@@ -12,3 +15,4 @@ def init_routes(app):
     app.register_blueprint(upcoming_events_bp, url_prefix='/events')
     app.register_blueprint(dia_events_bp, url_prefix='/events')
     app.register_blueprint(school_mappings_bp) 
+    app.register_blueprint(district_bp)
