66df712e69311ee393ab64513689f7348fd8a31f
Jon Lane <admiralorbiter@gmail.com>
2025-03-20 12:34:27 -0500

    fixed an issue with env

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # config.py
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # This line is crucial for loading the .env file
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
